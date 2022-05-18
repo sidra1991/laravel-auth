cd5c3fb2de4b0a81b4e469211ac8759262bb5560
anthony <diomede1991@libero.it>
2022-05-18 16:42:23 +0200

    mvc

---
+++ database/factories/PressFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\press;
+use Faker\Generator as Faker;
+
+$factory->define(press::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/seeds/PressSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PressSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
